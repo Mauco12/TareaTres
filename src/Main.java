02e32f8145f9d907e7c5a98c2cee5ab52adfb8c8
Mauricio <mauriciorodriguez90@aragon.unam.mx>
2023-03-20 16:12:10 -0600

    Tarea de juego del gato.

--- src/Main.java
+++ src/Main.java
@@ -11,17 +11,14 @@ public class Main {
 
         for (int i = 0; i < entrada.length(); i++) {
             char c = entrada.charAt(i);
-            if (c != 'X' && c != 'O' && c != '_') {
-                System.out.println("Solo puedes ingresar estos digitos: X,O,_ ");
-                return;
-            }
+
         }
 
-        System.out.println(" ---------- ");
+        System.out.println("  _________ ");
         System.out.println(" | " + entrada.charAt(0) + "  " + entrada.charAt(1) + "  " + entrada.charAt(2) + " | ");
         System.out.println(" | " + entrada.charAt(3) + "  " + entrada.charAt(4) + "  " + entrada.charAt(5) + " | ");
         System.out.println(" | " + entrada.charAt(6) + "  " + entrada.charAt(7) + "  " + entrada.charAt(8) + " | ");
-        System.out.println(" ---------- ");
+        System.out.println("  _________");
 
     }
 }
